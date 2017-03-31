a5c59adde12a4a6ec67885a50e3e7b0db1b6e6f5
OscarGibson <oneostap@gmail.com>
2017-03-31 18:47:35 +0300

    create download class

---
+++ download.py
@@ -0,0 +1,43 @@
+import sys
+import youtube_dl
+
+class videoDownloader():
+    __options_pattern = ['username','password','outtmpl','format']
+
+    def __init__(self, **kwargs):
+        self.ydl_opts = self.__set_values(self.__options_pattern, kwargs)
+
+    def set_options(self, **kwargs):
+        self.ydl_opts = self.__set_values(self.__options_pattern, kwargs)
+
+    def __set_values(self, options_pattern, kwargs):
+        options = {}
+        for name, value in kwargs.items():
+            if (name in options_pattern):
+                options[name] = value
+        return options
+
+    def print_kw(self):
+        print(self.ydl_opts)
+
+    def download(self, url):
+        ydl_opts = self.ydl_opts
+        info_dict = None
+
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                #ydl.download([url])
+                info_dict = ydl.extract_info(url)
+        except:
+            print('ERROR: downloading fall')
+            sys.exit(1)
+        return info_dict
+
+    def get_info(self, info, *args):
+        all_info = {}
+
+        for a in args:
+            i = info.get(a, None)
+            if i: all_info[a] = i
+
+        return all_info
